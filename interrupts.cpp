3c57f52e18e33b0baf520f38c51c74859c255982
sonaihaghgooie <sonayka.h@gmail.com>
2025-10-04 18:08:00 -0400

    added more activities to isr and started on END_IO

--- interrupts.cpp
+++ interrupts.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv) {
 
     int simulation_time = 0; // this is the total duration of the sim
     const int context_time = 10; // context
-    const int isr_time = 1; // isr
+    const int isr_time = 40; // isr
 
     /******************************************************************/
 
@@ -49,8 +49,16 @@ int main(int argc, char** argv) {
             execution += std::to_string(simulation_time) + ", " + std::to_string(isr_time) + " SYSCALL: run the ISR for device " + std::to_string(duration_intr) + "\n";
             simulation_time += isr_time;
 
-            
+            execution += std::to_string(simulation_time) + ", " + std::to_string(isr_time) + " transfer data from device to memory\n";
+            simulation_time += isr_time;
 
+            int wait_time = delays[duration_intr];
+            execution += std::to_string(simulation_time) + ", " + std::to_string(wait_time) + " check for errors\n";
+
+        }
+
+        else if (activity == "END_IO") {
+            auto [execution_temp, simulation_time_temp] = intr_boilerplate(simulation_time, duration_intr, context_time, vectors);
         }
 
         /************************************************************************/
