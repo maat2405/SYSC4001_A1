d54c7855001d20dfa8ee85498cba7432f2d615c8
Maathusan Sathiendran <155667928+maat2405@users.noreply.github.com>
2025-10-04 15:42:13 -0400

    Added CPU and partial implementation of SYSCALL activities

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,6 +2,9 @@
  *
  * @file interrupts.cpp
  * @author Sasisekhar Govind
+ * 
+ * @author Maathusan Sathiendran
+ * @author Sonai Haghgooie
  *
  */
 
@@ -20,17 +23,30 @@ int main(int argc, char** argv) {
 
     /******************ADD YOUR VARIABLES HERE*************************/
 
-
+    int simulation_time = 0; // this is the total duration of the sim
+    int context_time = 10; // context
 
     /******************************************************************/
 
     //parse each line of the input trace file
     while(std::getline(input_file, trace)) {
+        //activity will return from the file which kind, duration_intr is the duration of the interrupt
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
+        if (activity == "CPU") {
+            execution += std::to_string(simulation_time) + ", " + std::to_string(duration_intr) + ", CPU Burst\n";
+            simulation_time += duration_intr;
+        }
 
+        else if (activity == "SYSCALL") {
+            auto [execution_temp, simulation_time_temp] = intr_boilerplate(simulation_time, duration_intr, context_time, vectors);
+            execution += execution_temp;
+            simulation_time = simulation_time_temp;
+
+            
+        }
 
         /************************************************************************/
 
